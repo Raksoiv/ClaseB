46ddbd4e4329bb5102c3c05784a2be26cb31242c
Oscar Rencoret <o.rencoret@gmail.com>
2015-10-25 22:04:32 -0300

    Agegado reset de la camara con el f9 y corrección de la camara del auto

---
+++ Assets/Scripts/Oculus/OculusController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.VR.VRSettings.enabled = true;
+		UnityEngine.VR.InputTracking.Recenter();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("OculusReset")){
+			UnityEngine.VR.InputTracking.Recenter();
+		}
+	}
+}
