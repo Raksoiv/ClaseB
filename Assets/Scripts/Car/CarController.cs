4246598f353432dfd4b594d0404713791892fce6
Nicolás Aravena <mhobbit@gmail.com>
2015-11-04 01:10:17 -0300

    Closes #4

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -5,9 +5,9 @@
 
 public class CarController : MonoBehaviour {
 	public Transform steeringWheel;
-	private bool debugGUI = false;
+	private bool debugGUI = true;
 
-	Powertrain drivetrain;
+	Powertrain powertrain;
 
 	void UpdateSteeringwheelRotation(float deg){
 	//	steeringWheel.transform.localRotation.eulerAngles.z = deg;
@@ -15,7 +15,7 @@ public class CarController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		drivetrain = GetComponent<Powertrain> ();
+		powertrain = GetComponent<Powertrain> ();
 	}
 	
 	// Update is called once per frame
@@ -23,28 +23,48 @@ public class CarController : MonoBehaviour {
 		steeringWheel.transform.localRotation = Quaternion.Euler (0, 0, -Input.GetAxis ("Horizontal")*90);
 		if (Input.GetKeyDown (KeyCode.F12))
 			debugGUI = !debugGUI;
-	}
-	//Update for physics
-	void FixedUpdate(){
+
 		//Input de teclado
 		float steer = Input.GetAxis ("Horizontal");
-		float accelerate = Input.GetAxis ("Vertical");
+		float combinedInput = Input.GetAxis ("Vertical");
+		float clutch = Input.GetAxis ("Clutch");
 
-		drivetrain.Throttle = accelerate;
-		drivetrain.Brake = accelerate * -1;
+		powertrain.Throttle = Mathf.Clamp01(combinedInput);
+		powertrain.Brake = - Mathf.Clamp (combinedInput, -1, 0);
+		powertrain.Clutch = (clutch + 1)/2f;
 
+		
 		float finalAngle = steer * 45f;
-	
-		drivetrain.Steering = finalAngle;
+		
+		powertrain.Steering = finalAngle;
+
+		if (Input.GetButtonDown ("1stGear"))
+			powertrain.ShiftTo(1);
+		else if (Input.GetButtonDown ("2ndGear"))
+			powertrain.ShiftTo(2);
+		else if (Input.GetButtonDown ("3rdGear"))
+			powertrain.ShiftTo(3);
+		else if (Input.GetButtonDown ("4thGear"))
+			powertrain.ShiftTo(4);
+		else if (Input.GetButtonDown ("5thGear"))
+			powertrain.ShiftTo(5);
+		else if (Input.GetButtonDown ("6thGear"))
+			powertrain.ShiftTo(6);
+		else if (Input.GetButtonDown ("ReverseGear"))
+			powertrain.ShiftTo(7);
+		else if (Input.GetButtonUp ("1stGear") || Input.GetButtonUp ("2ndGear") || Input.GetButtonUp ("3rdGear") || Input.GetButtonUp ("4thGear") || Input.GetButtonUp ("5thGear") || Input.GetButtonUp ("6thGear") || Input.GetButtonUp ("ReverseGear"))
+			powertrain.ShiftTo(0);
 	}
 
 	void OnGUI() {
 		if (debugGUI) {
-			var speed = GetComponent<Rigidbody> ().velocity.magnitude * 3.6f;
+			float speed = GetComponent<Rigidbody> ().velocity.magnitude * 3.6f;
+			string rpm = powertrain.GetCurrentRPM().ToString();
+			string gear = (powertrain.GetCurrentGear() == 7)? "R" : powertrain.GetCurrentGear().ToString();
 			GUI.Box (new Rect (50, 50, 140, 55),
-			         "Velocidad: " + speed.ToString ("F0") + " Km/h" + System.Environment.NewLine +
-			         "RPM: " + drivetrain.GetCurrentRPM().ToString() + System.Environment.NewLine +
-			         "Cambio: " + drivetrain.GetCurrentGear().ToString() + System.Environment.NewLine
+			         "Speed : " + speed.ToString ("F0") + " Km/h" + System.Environment.NewLine +
+			         "RPM   : " + rpm + System.Environment.NewLine +
+			         "Gear  : " + gear + System.Environment.NewLine
 			);
 		}
 	}
--- Assets/Scripts/Car/Powertrain.cs
+++ Assets/Scripts/Car/Powertrain.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//Este componente simula y maneja: Motor + Transmision + Ruedas + Diferencial + Eje de Transmision = Powertrain,
+//Este componente simula y maneja: Motor + Transmision + Ruedas + Diferencial + Eje de Transmision + Frenos = Powertrain,
 //generando torque y aplicandoselo a las ruedas.
 
 public class Powertrain : MonoBehaviour {
 	[SerializeField]private AnimationCurve torqueCurve;                             //Curva de RPM/torque[Nm] del motor
-	[SerializeField]private float[] gearRatios = new float[7];                      //Relacion de reduccion de los cambios (0 = reversa)
+	[SerializeField]private float[] gearRatios = new float[8];                      //Relacion de reduccion de los cambios (0 = neutro, 7=reversa)
 	[SerializeField]private float finalGearRatio;                                   //Relacion de reduccion del diferencial
 	[SerializeField]private int minRPM;                                             //RPM de ralenti del motor (usado en cambios automaticos)
 	[SerializeField]private int maxRPM; 											//Redline
@@ -14,15 +14,19 @@ public class Powertrain : MonoBehaviour {
 	[SerializeField]private WheelCollider[] wheelColliders = new WheelCollider[4];  //WheelColliders: Delantera, Delantera, Trasera, Trasera
 	[SerializeField]private Transform[] tireMeshes = new Transform[4];              //Meshes de las ruedas (en el mismo orden que los WheelColliders)
 
-	[SerializeField]private bool isAutomatic = true;							
+	[SerializeField]private bool isAutomatic = false;							
 
 	[SerializeField]private int brakingForce;										//Fuerza de Frenado (en Newtons)
-	
-	int currentGear = 1;
+	[SerializeField]private int engineInertia;										//Inercia del motor ocupada para acelerar en neutro.
+	[SerializeField]private float transmissionEfficiency = 0.7f;					//Eficiencia del sistema de transmision
+
+
+	int currentGear = 0;
 	float throttle;
 	float brake;
+	float clutch;
 	float steering;
-	int RPM;
+	float engineRPM;
 
 	public float Throttle {
 		get {
@@ -39,6 +43,15 @@ public class Powertrain : MonoBehaviour {
 		}
 		set {
 			brake = value;
+		}
+	}
+
+	public float Clutch {
+		get {
+			return clutch;
+		}
+		set {
+			clutch = value;
 		}
 	}
 
@@ -63,63 +76,145 @@ public class Powertrain : MonoBehaviour {
 			
 		}
 	}
-	
+
+	float CalculateWheelTorque(){
+		//Calculando el torque con la ayuda de las formulas obtenidas desde:
+		//http://www.asawicki.info/Mirror/Car%20Physics%20for%20Games/Car%20Physics%20for%20Games.html
+		//http://forum.unity3d.com/threads/how-to-add-clutch-setting-in-truck-physics.65884/
+		
+		float wheelRPM = (wheelColliders [0].rpm + wheelColliders [1].rpm) / 2;
+		float disengagedEngineRPM = wheelRPM * finalGearRatio * gearRatios [currentGear]; //if clutch == 0
+		float engagedEngineRPM = engineInertia * throttle + engineRPM;                    //if clutch == 1 
+		
+		//RPM del motor dependiendo del estado del embrague
+		engineRPM = (1 - clutch) * engagedEngineRPM + clutch * disengagedEngineRPM;
+
+		//neutro == clutch = 1
+		if (currentGear == 0) 
+			engineRPM = engagedEngineRPM;
+
+		if (currentGear == 0 || clutch == 0){ 
+			if (throttle == 0)
+				engineRPM = engineRPM - engineInertia * 0.3f;
+			
+			if (engineRPM < minRPM) 
+				engineRPM = minRPM;
+		}
+
+		if (engineRPM > maxRPM) {
+			if (throttle != 0) engineRPM = maxRPM; 
+		}
+		
+		//Calcular torque desde la curva.
+		float engineTorque = torqueCurve.Evaluate (engineRPM);
+		float drive = throttle * (clutch + 1) / 2;
+		float driveTorque =  drive * engineTorque * gearRatios [currentGear] * finalGearRatio * transmissionEfficiency;	
+
+		return driveTorque;
+	}
+
+	void ApplyBrakes(){
+		foreach (WheelCollider wheel in wheelColliders) {
+			float brakeForce = brakingForce * brake;
+			float engineBrake = (1 - throttle) * engineRPM * 0.74f * gearRatios [currentGear] * finalGearRatio * transmissionEfficiency;
+			wheel.brakeTorque =  brakeForce * engineBrake;
+		}
+	}
+
 	void Update(){
 		UpdateWheelMeshesPositions ();
 	}
-	
+	void FixedUpdate () {
+		ApplyBrakes ();
+		
+		//pasar torque a las ruedas
+		float driveTorque = CalculateWheelTorque ();
+		
+		//Diferencial: TO DO, considerado en el desarrollo de la direccion
+		wheelColliders[0].motorTorque = driveTorque/2;
+		wheelColliders[1].motorTorque = driveTorque/2;
+		
+		//aplicar rotacion
+		wheelColliders [0].steerAngle = steering; 
+		wheelColliders [1].steerAngle = steering; 
+	}
+
+	/*
 	void FixedUpdate () {
 		//Calculando el torque con la ayuda de las formulas obtenidas desde:
 		//http://www.asawicki.info/Mirror/Car%20Physics%20for%20Games/Car%20Physics%20for%20Games.html
-		
+		//http://forum.unity3d.com/threads/how-to-add-clutch-setting-in-truck-physics.65884/
+
 		float wheelRPM = (wheelColliders [0].rpm + wheelColliders [1].rpm) / 2;
-		//Calcular RPM del motor cuando el embrague esta desactivado
-		//if (clutch != 0)
-		float engineRPM = wheelRPM * finalGearRatio * gearRatios [currentGear];
 
-		//test
-		if (engineRPM < minRPM)
-			engineRPM = minRPM;
+		//RPM del motor cuando el embrague esta desactivado
+		float disengagedEngineRPM = wheelRPM * finalGearRatio * gearRatios [currentGear];
 
+		//RPM del motor cuando el embrague esta completamente activado == 1 (o en neutro)
+		float engagedEngineRPM = engineInertia * throttle + engineRPM;
+
+
+		//RPM del motor dependiendo del estado del embrague
+		engineRPM = (1 - clutch) * engagedEngineRPM + clutch * disengagedEngineRPM;
+
+		if (currentGear == 0) {
+			engineRPM = engagedEngineRPM;
+		}
+
+
+		if (engineRPM < minRPM) {
+			//mantener RPM de ralenti en los casos necesarios
+			if (clutch == 0 || currentGear == 0)
+				engineRPM = minRPM;
+			else if (clutch < 0.9)
+				;//AQUI SE PUEDE DETENER EL MOTOR
+		} 
+		else if (engineRPM > maxRPM)
+			engineRPM = maxRPM;
+
+		
 		//Calcular torque desde la curva.
-		float totalMotorTorque = torqueCurve.Evaluate (engineRPM) * gearRatios [currentGear] * finalGearRatio * 0.7f * throttle;
+		float engineTorque = torqueCurve.Evaluate (engineRPM);
+		print (engineRPM + ":" +engineTorque);
+		float drive = ((1 - clutch) + throttle) / 2;
+		float driveTorque =  clutch * engineTorque * gearRatios [currentGear] * finalGearRatio * transmissionEfficiency;
 		
 		
 		//pasar torque a las ruedas
 		//Diferencial: TO DO, considerado en el desarrollo de la direccion
-		wheelColliders[0].motorTorque = totalMotorTorque/2;
-		wheelColliders[1].motorTorque = totalMotorTorque/2;
+		wheelColliders[0].motorTorque = driveTorque/2;
+		wheelColliders[1].motorTorque = driveTorque/2;
 
 		//aplicar rotacion
 		wheelColliders [0].steerAngle = steering; 
 		wheelColliders [1].steerAngle = steering; 
 
 		// Cambios automaticos.
-		if (isAutomatic)
-		{
+		if (isAutomatic) {
 			if (engineRPM >= maxRPM)
 				ShiftUp ();
 			else if (engineRPM <= minRPM * 1.1f && currentGear > 2)
 				ShiftDown ();
 			if (throttle < 0 && engineRPM <= minRPM)
-				currentGear = (currentGear == 0?2:0);
+				currentGear = (currentGear == 0 ? 2 : 0);
+		} 
+		//Cosas cambios manuales
+		if (!isAutomatic) {
+
 		}
-		
-		RPM = (int)engineRPM;
 
 		//Aplicar Frenos
 		if (brake > 0) {
 			foreach (WheelCollider wheel in wheelColliders) {
 				wheel.brakeTorque = brakingForce * brake / wheelColliders.Length;
 			}
-			Debug.Log (brake);
 		} else {
 			foreach (WheelCollider wheel in wheelColliders){
 				wheel.brakeTorque = 0;
 			}
 		}
 	}
-	
+	*/
 	public void ShiftUp(){
 		if (currentGear < gearRatios.Length - 1)
 			currentGear++; 
@@ -129,13 +224,19 @@ public class Powertrain : MonoBehaviour {
 			currentGear--;
 	}
 	public void ShiftTo(int targetGear){
-		if (targetGear >= 0 && targetGear <= gearRatios.Length)
-			currentGear = targetGear;
+		//currentGear = targetGear;
+		if (clutch < 0.2) {
+			if (targetGear >= 0 && targetGear <= gearRatios.Length)
+				currentGear = targetGear;
+		}
+		else {
+			currentGear = 0;
+		}
 	}
 	public int GetCurrentGear(){
 		return currentGear;
 	}
 	public int GetCurrentRPM(){
-		return RPM;
+		return (int)engineRPM;
 	}
 }
