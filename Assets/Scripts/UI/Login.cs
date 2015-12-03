4d3af4ac91d42aca3e4eb9981e3e19996657d8c8
Jorge Villagrán <insive@outlook.es>
2015-12-03 02:52:35 -0300

    Interfaz Terminada
    La interfaz de inicio esta terminada, falta comunicarla con la Web para
    obtener el rut

---
+++ Assets/Scripts/UI/Login.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+    public void ComprobarDatos()
+    {
+
+        if (false)
+        {
+            Application.LoadLevel("ArteTest");
+        }
+        else
+        {
+            Text txt = gameObject.GetComponent<Text>();
+            txt.text = "Rut incorrecto";
+        }
+    }
+}
